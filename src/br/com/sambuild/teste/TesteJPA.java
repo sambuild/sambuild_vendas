629b613bdb83e1b7be73983cc046fdd6baa8e471
Jose Adriano <adriano.skilo@gmail.com>
2014-04-28 00:15:21 -0300

    Testando o commit depois do import do github

--- src/br/com/sambuild/teste/TesteJPA.java
+++ src/br/com/sambuild/teste/TesteJPA.java
@@ -2,7 +2,7 @@ package br.com.sambuild.teste;
 
 import javax.persistence.EntityManager;
 
-import br.com.sambuild.modelo.Contato;
+import br.com.sambuild.modelo.Cliente;
 import br.com.sambuild.util.JPAUtil;
 
 public class TesteJPA {
@@ -10,9 +10,9 @@ public class TesteJPA {
 	public static void main(String[] args) {
 		EntityManager em = new JPAUtil().getEntityManager();
 		
-		Contato contato = em.find(Contato.class, 3L);
+		Cliente cliente = em.find(Cliente.class, 1L);
 		
-		System.out.println(contato.getDescricao());
+		System.out.println(cliente.getNome());
 		
 		em.close();
 	}
