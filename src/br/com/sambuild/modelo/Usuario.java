2e7e5d470fe9c1f33e9655e17c44cd8bdb73f73f
Jose Adriano <adriano.skilo@gmail.com>
2014-05-11 14:39:12 -0300

    Corrigindo modelo usuario

--- src/br/com/sambuild/modelo/Usuario.java
+++ src/br/com/sambuild/modelo/Usuario.java
@@ -1,74 +1,21 @@
 package br.com.sambuild.modelo;
 
-<<<<<<< HEAD
+import java.util.Calendar;
+
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Query;
-
-import br.com.sambuild.util.JPAUtil;
-=======
-import java.util.Calendar;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
->>>>>>> 0857c067250a770e9f0d22acadde142f7b17decb
+
+import br.com.sambuild.util.JPAUtil;
 
 @Entity
 public class Usuario {
 
-<<<<<<< HEAD
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long id;
-
-	private String login;
-
-	private String senha;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getLogin() {
-		return login;
-	}
-
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	public boolean existe() {
-		EntityManager em = new JPAUtil().getEntityManager();
-		
-		Query query = em.createQuery(
-				"select u from Usuario u where u.login = :pLogin and u.senha = :pSenha")
-			.setParameter("pLogin", getLogin())
-			.setParameter("pSenha", getSenha());
-	
-		boolean encontrado = !query.getResultList().isEmpty();
-	
-		return encontrado;
-    }
-=======
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -144,5 +91,16 @@ public class Usuario {
         this.ativo = ativo;
     }
 
->>>>>>> 0857c067250a770e9f0d22acadde142f7b17decb
+    public boolean existe() {
+		EntityManager em = JPAUtil.getEntityManager();
+
+		Query query = em.createQuery(
+				"select u from Usuario u where u.email = :pEmail and u.senha = :pSenha")
+			.setParameter("pEmail", getEmail())
+			.setParameter("pSenha", getSenha());
+
+		boolean encontrado = !query.getResultList().isEmpty();
+
+		return encontrado;
+    }
 }
